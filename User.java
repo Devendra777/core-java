deae3646ae3922523c43924dc6da780b28369334
X-workz <xworkzodc@gmail.com>
2022-09-16 13:18:21 +0530

    committing java files

---
+++ User.java
@@ -0,0 +1,16 @@
+class User{
+
+
+  static String cards[]=  {"Platinum Cerdit cards","millenia credit / debit cards","Kotak Urbane card"};
+   
+   public static void main(String a[]){
+   
+        for(int a = 0 ; a < cards.length;a++){
+	 System.out.println(cards[a]);
+		 }
+   
+   }
+
+
+
+}
