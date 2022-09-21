2f233e5a12f62970224061e8ec929306f6927209
X-workz <xworkzodc@gmail.com>
2022-09-21 12:44:57 +0530

    Committing method overloading files

---
+++ SwiggyTester.java
@@ -0,0 +1,18 @@
+class SwiggyTester{
+
+
+
+ public static void main(String a[]){
+ 
+ // invoking a method
+   Swiggy.takeOrder("mosaranna") ; 
+   Swiggy.takeOrder("mosaranna" , 67) ; 
+  System.out.println("Your Final Is Ready... Enjoyy");
+ 
+ 
+ }
+
+
+
+
+}
