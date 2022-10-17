c2b3e541c7caf67709c1416cf0bed18a02dff390
X-workz <xworkzodc@gmail.com>
2022-10-17 13:47:44 +0530

    committing object files

---
+++ airport-management-system/src/com/xworkz/airport/Airport.java
@@ -0,0 +1,14 @@
+package com.xworkz.airport;
+
+public class Airport {
+
+	// instance
+	static String name = "BIA";
+
+	public static void main(String[] args) {
+
+		// System.out.println(Airport.name) ;
+        System.out.println(Airport.name);
+
+	}
+}
